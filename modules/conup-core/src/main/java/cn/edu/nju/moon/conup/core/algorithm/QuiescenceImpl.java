6a139200915aa8963861b1ee99e5b0e746f73735
jiang.wang88 <jiang.wang88@gmail.com>
2012-10-30 11:51:34 +0000

    conup-core module

---
+++ modules/conup-core/src/main/java/cn/edu/nju/moon/conup/core/algorithm/QuiescenceImpl.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package cn.edu.nju.moon.conup.core.algorithm;
+
+import java.util.Set;
+
+import cn.edu.nju.moon.conup.spi.datamodel.Algorithm;
+
+
+/**
+ * @author nju
+ *
+ */
+public class QuiescenceImpl implements Algorithm {
+
+	/* (non-Javadoc)
+	 * @see cn.edu.nju.moon.conup.core.algorithm.Algorithm#analyze(java.lang.String, java.lang.String, java.util.Set, java.util.Set)
+	 */
+	public void analyze(String txStatus, String txID, Set<String> futureC,
+			Set<String> pastC) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ modules/conup-core/src/main/java/cn/edu/nju/moon/conup/core/ondemand/VersionConsistencyOndemandImpl.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package cn.edu.nju.moon.conup.core.ondemand;
+
+import cn.edu.nju.moon.conup.spi.datamodel.OndemandSetup;
+import cn.edu.nju.moon.conup.spi.datamodel.Scope;
+
+
+/**
+ * 
+ * The on-demand setup process of Version-consistency algorithm
+ * @author nju
+ *
+ */
+public class VersionConsistencyOndemandImpl implements OndemandSetup {
+
+	/* (non-Javadoc)
+	 * @see cn.edu.nju.moon.conup.core.ondemand.Ondemand#ondemand(java.lang.String, java.lang.String)
+	 */
+	public void ondemand(String targetComponent, String freenessSetup, Scope scope) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see cn.edu.nju.moon.conup.core.ondemand.Ondemand#isOndemandDone()
+	 */
+	public boolean isOndemandDone() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
