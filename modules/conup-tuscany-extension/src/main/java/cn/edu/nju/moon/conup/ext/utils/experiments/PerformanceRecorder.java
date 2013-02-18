85bef7281b02260a40921e6aa48f565f60faa622
valerio.panzica <valerio.panzica@gmail.com>
2013-02-18 16:06:55 +0000

    refactor the performance Recorder and create and set up an initial experiment class

---
+++ modules/conup-tuscany-extension/src/main/java/cn/edu/nju/moon/conup/ext/utils/experiments/PerformanceRecorder.java
@@ -0,0 +1,46 @@
+package cn.edu.nju.moon.conup.ext.utils.experiments;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
+
+import cn.edu.nju.moon.conup.ext.utils.experiments.model.Experiment;
+
+/**
+ * @author JiangWang<jiang.wang88@gmail.com>
+ *
+ */
+public class PerformanceRecorder {
+    //private static ExecutionRecorder exeRecorder = new ExecutionRecorder();
+	private static Map<String, PerformanceRecorder> componentIdentifierToPerformanceRecorderMap = new ConcurrentSkipListMap<String, PerformanceRecorder>();
+	
+	
+	private long startTime;
+	
+	private long endTime;
+	
+	private PerformanceRecorder(){
+		
+	}
+
+	public static PerformanceRecorder getInstance(String compIdentifier){
+		synchronized (componentIdentifierToPerformanceRecorderMap) {
+			if(!componentIdentifierToPerformanceRecorderMap.containsKey(compIdentifier)){
+				
+				componentIdentifierToPerformanceRecorderMap.put(compIdentifier, new PerformanceRecorder());
+			} 
+			return componentIdentifierToPerformanceRecorderMap.get(compIdentifier);
+		}
+	}
+	
+	
+	public void updateIsDone(long endTime){
+		this.endTime = endTime;
+		String data ="update has taken time: " + (this.endTime - startTime) / 1000000.0 + " ms";
+		Experiment.getInstance().writeToFile(data);
+	}
+	
+	public void updateReceived(long startTime){
+		this.startTime = startTime;
+	}
+	
+}
