3a656d3f24d2c0674382333003faab1283255016
valerio.panzica <valerio.panzica@gmail.com>
2013-02-18 16:11:10 +0000

    revert the travel sample into a working example

--- samples/conup-travel-sample/fullapp-bank/src/main/java/com/tuscanyscatours/bank/Bank.java
+++
@@ -1,8 +0,0 @@
-package com.tuscanyscatours.bank;
-
-import org.oasisopen.sca.annotation.Remotable;
-
-@Remotable
-public interface Bank {
-	double getExchangeRate(String fromCurrencyCode, String toCurrencyCode);
-}
