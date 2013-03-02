83d0b991ab526b658e7defa75e73e69ee2b38e15
rgc.nju.cs@gmail.com <rgc.nju.cs@gmail.com@3cfdaae8-dac9-9527-6ad1-29de82a2e67e>
2013-03-02 12:48:07 +0000

    add specific code to augment shell for travel-experiment

---
+++ trunk/modules/shell/src/main/java/com/tuscanyscatours/CoordinationVisitorThread.java
@@ -0,0 +1,23 @@
+package com.tuscanyscatours;
+
+import org.apache.tuscany.sca.Node;
+import org.oasisopen.sca.NoSuchServiceException;
+
+import com.tuscanyscatours.coordination.Coordination;
+
+public class CoordinationVisitorThread extends Thread{
+	private Node node;
+
+	public CoordinationVisitorThread(Node node) {
+		this.node = node;
+	}
+
+	public void run() {
+		try {
+			Coordination scaTour = node.getService(Coordination.class, "Coordination#service-binding(Coordination/Coordination)");
+			scaTour.coordinate();
+		} catch (NoSuchServiceException e) {
+			e.printStackTrace();
+		}
+	}// END RUN()
+}
