5522c06d1b3eefd7873a7d881b5512694ceb1d54
rgc.nju.cs@gmail.com <rgc.nju.cs@gmail.com@3cfdaae8-dac9-9527-6ad1-29de82a2e67e>
2013-04-04 02:45:06 +0000

    fix testcase bug

---
+++ trunk/modules/conup-tuscany-extension/src/test/java/cn/edu/nju/moon/conup/ext/utils/experiments/model/ExperimentTest.java
@@ -0,0 +1,33 @@
+package cn.edu.nju.moon.conup.ext.utils.experiments.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExperimentTest {
+	
+	private Experiment exp = null;
+
+	@Before
+	public void setUp() throws Exception {
+		exp = Experiment.getInstance();
+	}
+
+	@Test
+	public void testGetExpSetting() {
+	}
+
+	@Test
+	public void testWriteDisruptionToFile() {
+	}
+
+	@Test
+	public void testWriteToFile() {
+	}
+
+	@Test
+	public void testClose() {
+	}
+
+}
