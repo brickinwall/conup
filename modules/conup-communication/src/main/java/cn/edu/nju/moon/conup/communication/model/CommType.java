42bcf41d7c9364711cab3bb7af162b2ac7eb3645
rgc.nju.cs <rgc.nju.cs@gmail.com>
2013-07-16 01:32:53 +0000

    move RequestObject, ResponseObject, CommType to conup-spi

--- modules/conup-communication/src/main/java/cn/edu/nju/moon/conup/communication/model/CommType.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.nju.moon.conup.communication.model;
-
-public enum CommType {
-	SYN,
-	ASYN,
-	ONEWAY;
-}
--- modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/datamodel/ResponseObject.java
+++ modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/datamodel/ResponseObject.java
@@ -1,40 +1,30 @@
-package cn.edu.nju.moon.conup.communication.model;
+package cn.edu.nju.moon.conup.spi.datamodel;
 
 import java.io.Serializable;
 
-import cn.edu.nju.moon.conup.spi.datamodel.MsgType;
 
 /**
  * @author rgc
  */
-public class RequestObject implements Serializable {
-	
-	private static final long serialVersionUID = -2202113397807809095L;
-	
+public class ResponseObject implements Serializable {
+
+	private static final long serialVersionUID = 1611210140415769089L;
+
 	private String srcIdentifier;
 	private String targetIdentifier;
 	// the protocol type can be CONSISTENCY, QUIESCENCE and TRANQUILLITY
 	private String protocol;
 	private MsgType msgType;
 	private String payload;
-	private CommType commType;
-
-	public CommType getCommType() {
-		return commType;
-	}
-
-	public void setCommType(CommType commType) {
-		this.commType = commType;
-	}
 
 	public MsgType getMsgType() {
 		return msgType;
 	}
-	
+
 	public void setMsgType(MsgType msgType) {
 		this.msgType = msgType;
 	}
-	
+
 	public String getSrcIdentifier() {
 		return srcIdentifier;
 	}
@@ -66,11 +56,12 @@ public class RequestObject implements Serializable {
 	public void setPayload(String payload) {
 		this.payload = payload;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "RequestObject: srcIdentifier: " + srcIdentifier + " targetIdentifier: " + targetIdentifier +
-				" protocol: " + protocol + " payload: " + payload;
+		return "ResponseObject: srcIdentifier: " + srcIdentifier
+				+ " targetIdentifier: " + targetIdentifier + " protocol: "
+				+ protocol + " payload: " + payload;
 	}
 
 }
