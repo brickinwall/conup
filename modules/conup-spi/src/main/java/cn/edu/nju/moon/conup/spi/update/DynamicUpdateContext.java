24bcab39ab230c9e5a635e74662f2c03c6dd40a7
rgc.nju.cs <rgc.nju.cs@gmail.com>
2013-07-16 01:39:09 +0000

    change package name from spi.complifecycle to spi.update

--- modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/update/DynamicUpdateContext.java
+++ modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/update/DynamicUpdateContext.java
@@ -1,4 +1,4 @@
-package cn.edu.nju.moon.conup.spi.complifecycle;
+package cn.edu.nju.moon.conup.spi.update;
 
 import java.util.Set;
 
---
+++ modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/update/UpdateManager.java
@@ -0,0 +1,39 @@
+package cn.edu.nju.moon.conup.spi.update;
+
+import cn.edu.nju.moon.conup.spi.datamodel.RequestObject;
+import cn.edu.nju.moon.conup.spi.helper.OndemandSetupHelper;
+import cn.edu.nju.moon.conup.spi.manager.DynamicDepManager;
+
+public interface UpdateManager {
+
+	public void attemptToUpdate();
+
+	void checkFreeness(String hostComp);
+	
+	public boolean cleanupUpdate();
+	
+	public boolean executeUpdate();
+
+	public ComponentUpdator getCompUpdator();
+
+	public DynamicDepManager getDepMgr();
+	
+	public OndemandSetupHelper getOndemandSetupHelper();
+
+	public DynamicUpdateContext getUpdateCtx();
+
+	public void initOldRootTxs();
+
+	public boolean isDynamicUpdateRqstRCVD();
+
+	public String process(RequestObject reqObj);
+	
+	public void setCompUpdator(ComponentUpdator compUpdator);
+
+	public void setDepMgr(DynamicDepManager depMgr);
+
+	void setDynamicUpdateContext(DynamicUpdateContext updateCtx);
+
+	public void setOndemandSetupHelper(OndemandSetupHelper ondemandSetupHelper);
+	
+}
