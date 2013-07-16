e6d412102ca3ef9240d8a662f49c600bff0c796a
rgc.nju.cs@gmail.com <rgc.nju.cs@gmail.com@3cfdaae8-dac9-9527-6ad1-29de82a2e67e>
2013-07-16 07:45:36 +0000

    remove unnecessary comment in POJODynamicUpdateContext

--- trunk/modules/conup-tuscany-extension/src/main/java/cn/edu/nju/moon/conup/ext/datamodel/POJODynamicUpdateContext.java
+++ trunk/modules/conup-tuscany-extension/src/main/java/cn/edu/nju/moon/conup/ext/datamodel/POJODynamicUpdateContext.java
@@ -3,7 +3,7 @@ package cn.edu.nju.moon.conup.ext.datamodel;
 import java.util.Set;
 import java.util.logging.Logger;
 
-import cn.edu.nju.moon.conup.spi.complifecycle.DynamicUpdateContext;
+import cn.edu.nju.moon.conup.spi.update.DynamicUpdateContext;
 
 
 /** 
@@ -22,82 +22,52 @@ public class POJODynamicUpdateContext implements DynamicUpdateContext {
 	/**  root txs that should be responded with old version component */
 	private Set<String> algorithmOldRootTxs = null;
 	
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#isLoaded()
-	 */
 	@Override
 	public boolean isLoaded() {
 		return isLoaded;
 	}
 	
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#removeAlgorithmOldRootTx(java.lang.String)
-	 */
 	@Override
 	public void removeAlgorithmOldRootTx(String oldRootTx){
 		LOGGER.fine("in DynaUpdateCtx.removeAlgorithmOldRootTx(), rm " + oldRootTx);
 		algorithmOldRootTxs.remove(oldRootTx);
 	}
 	
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#setLoaded(boolean)
-	 */
 	@Override
 	public void setLoaded(boolean isLoaded) {
 		this.isLoaded = isLoaded;
 	}
 
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#getOldVerClass()
-	 */
 	@Override
 	public Class<?> getOldVerClass() {
 		return oldVerClass;
 	}
 
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#setOldVerClass(java.lang.Class)
-	 */
 	@Override
 	public void setOldVerClass(Class<?> oldVerClass) {
 		this.oldVerClass = oldVerClass;
 	}
 
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#getNewVerClass()
-	 */
 	@Override
 	public Class<?> getNewVerClass() {
 		return newVerClass;
 	}
 
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#setNewVerClass(java.lang.Class)
-	 */
 	@Override
 	public void setNewVerClass(Class<?> newVerClass) {
 		this.newVerClass = newVerClass;
 	}
 
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#getAlgorithmOldRootTxs()
-	 */
 	@Override
 	public Set<String> getAlgorithmOldRootTxs() {
 		return algorithmOldRootTxs;
 	}
 
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#setAlgorithmOldRootTxs(java.util.Set)
-	 */
 	@Override
 	public void setAlgorithmOldRootTxs(Set<String> algorithmOldRootTxs) {
 		this.algorithmOldRootTxs = algorithmOldRootTxs;
 	}
 	
-	/* (non-Javadoc)
-	 * @see cn.edu.nju.moon.conup.ext.datamodel.DynamicUpdateContext#isOldRootTxsInitiated()
-	 */
 	@Override
 	public boolean isOldRootTxsInitiated(){
 		return algorithmOldRootTxs != null;
