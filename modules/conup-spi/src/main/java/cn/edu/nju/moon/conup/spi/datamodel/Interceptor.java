9a31018f0e5abe1e969b66e7452290aafca9621d
rgc.nju.cs <rgc.nju.cs@gmail.com>
2014-01-02 08:03:32 +0000

    rewrite Interceptor interface to abstract class to make some method can be reused

--- modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/datamodel/Interceptor.java
+++ modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/datamodel/Interceptor.java
@@ -6,9 +6,11 @@ import org.apache.tuscany.sca.invocation.Message;
  * @author Guochao Ren<rgc.nju.cs@gmail.com>
  * @version Created time: Jul 28, 2013 11:03:20 PM
  */
-public interface Interceptor {
-	public Message invoke(Message msg);
+public abstract class Interceptor {
+	public abstract Message invoke(Message msg);
 	
-	public void update(Object arg);
+	public void update(Object arg){
+		
+	}
 	
 }
