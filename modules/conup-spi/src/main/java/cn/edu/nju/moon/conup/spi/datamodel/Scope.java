f475e54a3155e7af14b340ef13d859b4c4eff1df
rgc.nju.cs <rgc.nju.cs@gmail.com>
2014-02-15 12:47:42 +0000

    add isSpeficiedScope field to demonstrate whether current scope is specified by user, and also add getRootComp method to get the root comps of current comp in this scope.

--- modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/datamodel/Scope.java
+++ modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/datamodel/Scope.java
@@ -12,43 +12,58 @@ import java.util.Set;
  * @version Created time: Jul 28, 2013 11:04:09 PM
  */
 public class Scope {
-	
+
 	private Map<String, Set<String>> parentComponents;
 	private Map<String, Set<String>> subComponents;
-	
+
 	/** String "compA<compB" means that compB is a parent component of compA */
 	public static final String PARENT_SEPERATOR = "<";
 	/** String "compA>compC" means that compc is a sub component of compA */
 	public static final String SUB_SEPERATOR = ">";
-	/** String "compA+compB|compA-compC" means that compA has a parent component CompB 
-	 * 	and sub component compC*/
+	/**
+	 * String "compA+compB|compA-compC" means that compA has a parent component
+	 * CompB and sub component compC
+	 */
 	public static final String SCOPE_ENTRY_SEPERATOR = "#";
 	public static final String TARGET_IDENTIFIER = "TARGET_COMP";
 	/** */
 	public static final String TARGET_SEPERATOR = "@";
-	
-	//all components in current scope
+	public static final String SCOPE_FLAG_IDENTIFIER = "SCOPE_FLAG";
+	public static final String SCOPE_FLAG_SEPERATOR = "&";
+
+	// all components in current scope
 	private Set<String> components;
-	//target components that need to be dynamically updated
+	// target components that need to be dynamically updated
 	private Set<String> target;
 	
-	public Scope(){
+	// this field is used to demonstrate whether this scope is specified by administrator
+	// the default value is false, it means that the scope includes all the affected components
+	private boolean isSpecifiedScope = false;
+
+	public Scope() {
 		components = new HashSet<String>();
 		target = new HashSet<String>();
 		parentComponents = new HashMap<String, Set<String>>();
 		subComponents = new HashMap<String, Set<String>>();
 	}
-	
-	
-	public Set<String> getParentComponents(String current){
+
+	public boolean isSpecifiedScope() {
+		return isSpecifiedScope;
+	}
+
+	public void setSpecifiedScope(boolean isSpecifiedScope) {
+		this.isSpecifiedScope = isSpecifiedScope;
+	}
+
+	public Set<String> getParentComponents(String current) {
 		return parentComponents.get(current);
 	}
-	
-	public Set<String> getSubComponents(String current){
+
+	public Set<String> getSubComponents(String current) {
 		return subComponents.get(current);
 	}
-	
-	public boolean contains(String componentName){
+
+	public boolean contains(String componentName) {
 		return components.contains(componentName);
 	}
 
@@ -66,20 +81,22 @@ public class Scope {
 		this.target = target;
 	}
 
-	public void addComponent(String componentName, Set<String> parentComps, Set<String> subComps){
+	public void addComponent(String componentName, Set<String> parentComps,
+			Set<String> subComps) {
 		this.components.add(componentName);
-		if(parentComponents.get(componentName) == null){
+		if (parentComponents.get(componentName) == null) {
 			parentComponents.put(componentName, new HashSet<String>());
 		}
 		this.parentComponents.get(componentName).addAll(parentComps);
-		
-		if(subComponents.get(componentName) == null){
+
+		if (subComponents.get(componentName) == null) {
 			subComponents.put(componentName, new HashSet<String>());
 		}
 		this.subComponents.get(componentName).addAll(subComps);
 	}
-	
-	public void setComponent(String componentName, Set<String> parentComps, Set<String> subComps){
+
+	public void setComponent(String componentName, Set<String> parentComps,
+			Set<String> subComps) {
 		this.components.add(componentName);
 		this.parentComponents.put(componentName, parentComps);
 		this.parentComponents.put(componentName, subComps);
@@ -88,66 +105,98 @@ public class Scope {
 	@Override
 	public String toString() {
 		StringBuffer buffer = new StringBuffer();
-		for(Entry<String, Set<String>> entry : parentComponents.entrySet()){
-			for(String parent : entry.getValue()){
-				buffer.append(entry.getKey() + PARENT_SEPERATOR + parent + SCOPE_ENTRY_SEPERATOR);
+		for (Entry<String, Set<String>> entry : parentComponents.entrySet()) {
+			for (String parent : entry.getValue()) {
+				buffer.append(entry.getKey() + PARENT_SEPERATOR + parent
+						+ SCOPE_ENTRY_SEPERATOR);
 			}
+		}
+
+		for (Entry<String, Set<String>> entry : subComponents.entrySet()) {
+			for (String sub : entry.getValue()) {
+				buffer.append(entry.getKey() + SUB_SEPERATOR + sub
+						+ SCOPE_ENTRY_SEPERATOR);
+			}
+		}
+		for (String t : target) {
+			buffer.append(TARGET_IDENTIFIER + TARGET_SEPERATOR + t
+					+ SCOPE_ENTRY_SEPERATOR);
 		}
 		
-		for (Entry<String, Set<String>> entry : subComponents.entrySet()) {
-			for(String sub : entry.getValue()){
-				buffer.append(entry.getKey() + SUB_SEPERATOR + sub + SCOPE_ENTRY_SEPERATOR);
-			}
-		}
-		for(String t : target){
-			buffer.append(TARGET_IDENTIFIER + TARGET_SEPERATOR + t + SCOPE_ENTRY_SEPERATOR);
-		}
-		buffer.deleteCharAt(buffer.length()-1);
+		buffer.append(SCOPE_FLAG_IDENTIFIER + SCOPE_FLAG_SEPERATOR
+				+ isSpecifiedScope + SCOPE_ENTRY_SEPERATOR);
+		if(buffer.length() > 0)
+			buffer.deleteCharAt(buffer.length() - 1);
 		return buffer.toString();
 	}
-	
-	public static Scope inverse(String scopeString){
-		if(scopeString == null)
+
+	public static Scope inverse(String scopeString) {
+		if (scopeString == null)
 			return null;
-		
+
 		Scope scope = new Scope();
-		String [] split = scopeString.split(SCOPE_ENTRY_SEPERATOR);
-		
-		for(String entry : split){
-			if(entry.contains(PARENT_SEPERATOR)){
+		String[] split = scopeString.split(SCOPE_ENTRY_SEPERATOR);
+
+		for (String entry : split) {
+			if (entry.contains(PARENT_SEPERATOR)) {
 				String curComp = entry.split(PARENT_SEPERATOR)[0];
 				Set<String> parentComps = new HashSet<String>();
 				parentComps.add(entry.split(PARENT_SEPERATOR)[1]);
 				scope.addComponent(curComp, parentComps, new HashSet<String>());
-			} else if(entry.contains(SUB_SEPERATOR)){
+			} else if (entry.contains(SUB_SEPERATOR)) {
 				String curComp = entry.split(SUB_SEPERATOR)[0];
 				Set<String> subComps = new HashSet<String>();
 				subComps.add(entry.split(SUB_SEPERATOR)[1]);
 				scope.addComponent(curComp, new HashSet<String>(), subComps);
-			} else if(entry.contains(TARGET_SEPERATOR)){
+			} else if (entry.contains(TARGET_SEPERATOR)) {
 				String targetComp = entry.split(TARGET_SEPERATOR)[1];
 				scope.getTargetComponents().add(targetComp);
-			} else{
+			} else if(entry.contains(SCOPE_FLAG_SEPERATOR)) {
+				boolean isSpecifiedScope = Boolean.parseBoolean(entry.split(SCOPE_FLAG_SEPERATOR)[1]);
+				scope.setSpecifiedScope(isSpecifiedScope);
+			} else {
 				return null;
 			}
 		}
-		
-		if(scope.getTargetComponents().size() == 0){
+
+		if (scope.getTargetComponents().size() == 0) {
 			return null;
 		}
-		
+
 		return scope;
 	}
-
 
 	public boolean isTarget(String compName) {
 		return target.contains(compName);
 	}
 
-
 	public boolean isRootComp(String hostComp) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
+	/**
+	 * 
+	 * @param curComp
+	 * @return
+	 */
+	public Set<String> getRootComp(String curComp) {
+		Set<String> rootComps = new HashSet<String>();
+
+		return calcRootComps(curComp, rootComps);
+	}
+
+	private Set<String> calcRootComps(String curComp, Set<String> rootComps) {
+		if(parentComponents.get(curComp).size() == 0){
+			rootComps.add(curComp);
+			return rootComps;
+		}
+		
+		for(String comp : parentComponents.get(curComp)){
+			calcRootComps(comp, rootComps);
+		}
+		
+		return rootComps;
+	}
+
 }
--- modules/conup-spi/src/test/java/cn/edu/nju/moon/conup/spi/datamodel/ScopeTest.java
+++ modules/conup-spi/src/test/java/cn/edu/nju/moon/conup/spi/datamodel/ScopeTest.java
@@ -11,10 +11,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 /**
  * @author JiangWang<jiang.wang88@gmail.com>
- *
+ * 
  */
 public class ScopeTest {
 
@@ -40,45 +39,52 @@ public class ScopeTest {
 		Set<String> parentComps = new HashSet<String>();
 		Set<String> subComps = new HashSet<String>();
 		Set<String> targetComps = new HashSet<String>();
-		//auth component
+		// auth component
 		parentComps.add("ProcComponent");
 		parentComps.add("PortalComponent");
 		scope.addComponent("AuthComponent", parentComps, subComps);
-		//proc component
+		// proc component
 		parentComps.clear();
 		subComps.clear();
 		parentComps.add("PortalComponent");
 		subComps.add("AuthComponent");
 		scope.addComponent("ProcComponent", parentComps, subComps);
-		//portao component
+		// portal component
 		parentComps.clear();
 		subComps.clear();
 		subComps.add("AuthComponent");
 		subComps.add("ProcComponent");
 		scope.addComponent("PortalComponent", parentComps, subComps);
-		//target component
+		// target component
 		targetComps.add("AuthComponent");
 		scope.setTarget(targetComps);
-		
+
 		String real = scope.toString();
+		System.out.println(real);
 		assertTrue(real.contains("ProcComponent<PortalComponent"));
 		assertTrue(real.contains("AuthComponent<ProcComponent"));
 		assertTrue(real.contains("AuthComponent<PortalComponent"));
 		assertTrue(real.contains("ProcComponent>AuthComponent"));
-		assertTrue(real.contains(Scope.TARGET_IDENTIFIER + Scope.TARGET_SEPERATOR + "AuthComponent"));
+		assertTrue(real.contains(Scope.TARGET_IDENTIFIER
+				+ Scope.TARGET_SEPERATOR + "AuthComponent"));
 		assertTrue(!real.endsWith(Scope.SCOPE_ENTRY_SEPERATOR));
 	}
-	
+
 	@Test
-	public void testInverse(){
-		String toString = "ProcComponent<PortalComponent#" +
-						  "AuthComponent<ProcComponent#" +
-						  "AuthComponent<PortalComponent#" +
-						  "ProcComponent>AuthComponent#" +
-						  "PortalComponent>ProcComponent#" +
-						  "PortalComponent>AuthComponent#" + 
-						  "TARGET_COMP@AuthComponent";
+	public void testInverse() {
+		String toString = "ProcComponent<PortalComponent#"
+				+ "AuthComponent<ProcComponent#"
+				+ "AuthComponent<PortalComponent#"
+				+ "ProcComponent>AuthComponent#"
+				+ "PortalComponent>ProcComponent#"
+				+ "PortalComponent>AuthComponent#"
+				+ "TARGET_COMP@AuthComponent#"
+				+ "SCOPE_FLAG&true";
 		Scope scope = Scope.inverse(toString);
+		
+		// check isSpecified flag
+		assertTrue(scope.isSpecifiedScope());
+		
 		Set<String> authParent = scope.getParentComponents("AuthComponent");
 		Set<String> authSub = scope.getSubComponents("AuthComponent");
 		Set<String> procParent = scope.getParentComponents("ProcComponent");
@@ -86,44 +92,169 @@ public class ScopeTest {
 		Set<String> portalParent = scope.getParentComponents("PortalComponent");
 		Set<String> portalSub = scope.getSubComponents("PortalComponent");
 		Set<String> targetComps = scope.getTargetComponents();
-		
+
 		Set<String> expectParent = new HashSet<String>();
 		Set<String> expectSub = new HashSet<String>();
 		Set<String> expectTarget = new HashSet<String>();
-		
-		//auth component
+
+		// auth component
 		expectParent.clear();
 		expectSub.clear();
 		expectParent.add("ProcComponent");
 		expectParent.add("PortalComponent");
 		assertEquals(authParent, expectParent);
 		assertEquals(authSub, expectSub);
-		
-		//proc component
+
+		// proc component
 		expectParent.clear();
 		expectSub.clear();
 		expectParent.add("PortalComponent");
 		expectSub.add("AuthComponent");
 		assertEquals(procParent, expectParent);
 		assertEquals(procSub, expectSub);
-		
-		//portal component
+
+		// portal component
 		expectParent.clear();
 		expectSub.clear();
 		expectSub.add("AuthComponent");
 		expectSub.add("ProcComponent");
 		assertEquals(portalParent, expectParent);
 		assertEquals(portalSub, expectSub);
-		
-		//target components
+
+		// target components
 		expectTarget.add("AuthComponent");
 		assertTrue(targetComps.equals(expectTarget));
-		
-		
-		//abnormal cases
+
+		// abnormal cases
 		assertNull(Scope.inverse(null));
 		assertNull(Scope.inverse("#abc"));
 		assertNull(Scope.inverse("#"));
 	}
-	
+
+	@Test
+	public void testGetRootComp() {
+		// TEST CASE 1
+		Scope scope = new Scope();
+		Set<String> parentComps = new HashSet<String>();
+		Set<String> subComps = new HashSet<String>();
+		Set<String> targetComps = new HashSet<String>();
+		// auth component
+		parentComps.add("ProcComponent");
+		parentComps.add("PortalComponent");
+		scope.addComponent("AuthComponent", parentComps, subComps);
+		// proc component
+		parentComps.clear();
+		subComps.clear();
+		parentComps.add("PortalComponent");
+		subComps.add("AuthComponent");
+		scope.addComponent("ProcComponent", parentComps, subComps);
+		// portal component
+		parentComps.clear();
+		subComps.clear();
+		subComps.add("AuthComponent");
+		subComps.add("ProcComponent");
+		scope.addComponent("PortalComponent", parentComps, subComps);
+		// target component
+		targetComps.add("AuthComponent");
+		scope.setTarget(targetComps);
+
+		Set<String> rootComps = scope.getRootComp("AuthComponent");
+		assertTrue(rootComps.size() == 1);
+		assertTrue(rootComps.contains("PortalComponent"));
+
+		rootComps = scope.getRootComp("ProcComponent");
+		assertTrue(rootComps.size() == 1);
+		assertTrue(rootComps.contains("PortalComponent"));
+
+		// TEST CASE 2
+		parentComps.clear();
+		subComps.clear();
+		scope = new Scope();
+
+		// D component
+		parentComps.add("C");
+		parentComps.add("E");
+		scope.addComponent("D", parentComps, subComps);
+
+		// C component
+		parentComps.clear();
+		subComps.clear();
+		parentComps.add("B");
+		subComps.add("D");
+		scope.addComponent("C", parentComps, subComps);
+
+		// E component
+		parentComps.clear();
+		subComps.clear();
+		parentComps.add("B");
+		subComps.add("D");
+		scope.addComponent("E", parentComps, subComps);
+
+		// B component
+		parentComps.clear();
+		subComps.clear();
+		subComps.add("C");
+		subComps.add("E");
+		scope.addComponent("B", parentComps, subComps);
+
+		rootComps = scope.getRootComp("D");
+		assertTrue(rootComps.size() == 1);
+		assertTrue(rootComps.contains("B"));
+
+		rootComps = scope.getRootComp("C");
+		assertTrue(rootComps.size() == 1);
+		assertTrue(rootComps.contains("B"));
+
+		rootComps = scope.getRootComp("E");
+		assertTrue(rootComps.size() == 1);
+		assertTrue(rootComps.contains("B"));
+
+		// TEST CASE 3
+		parentComps.clear();
+		subComps.clear();
+		scope = new Scope();
+
+		// D component
+		parentComps.add("C");
+		parentComps.add("E");
+		scope.addComponent("D", parentComps, subComps);
+
+		// C component
+		parentComps.clear();
+		subComps.clear();
+		parentComps.add("B");
+		subComps.add("D");
+		scope.addComponent("C", parentComps, subComps);
+
+		// E component
+		parentComps.clear();
+		subComps.clear();
+		parentComps.add("A");
+		subComps.add("D");
+		scope.addComponent("E", parentComps, subComps);
+
+		// B component
+		parentComps.clear();
+		subComps.clear();
+		subComps.add("C");
+		scope.addComponent("B", parentComps, subComps);
+
+		// A component
+		parentComps.clear();
+		subComps.clear();
+		subComps.add("E");
+		scope.addComponent("A", parentComps, subComps);
+		
+		rootComps = scope.getRootComp("D");
+		assertTrue(rootComps.size() == 2);
+		assertTrue(rootComps.contains("B"));
+		assertTrue(rootComps.contains("A"));
+		
+		rootComps = scope.getRootComp("E");
+		assertTrue(rootComps.size() == 1);
+		assertTrue(!rootComps.contains("B"));
+		assertTrue(rootComps.contains("A"));
+
+		System.out.println(rootComps);
+	}
 }
