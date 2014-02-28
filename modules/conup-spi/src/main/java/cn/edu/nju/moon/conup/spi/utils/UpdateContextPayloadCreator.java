0fe58ee0fc5043b6a9b7631547388fbba60e07cb
rgc.nju.cs <rgc.nju.cs@gmail.com>
2014-02-28 06:33:25 +0000

    UpdateContextPayloadCreator changes: overload createPayload method, create a payload which contains scope

--- modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/utils/UpdateContextPayloadCreator.java
+++ modules/conup-spi/src/main/java/cn/edu/nju/moon/conup/spi/utils/UpdateContextPayloadCreator.java
@@ -24,12 +24,13 @@ public class UpdateContextPayloadCreator {
 			String targetCompIdentifier, String baseDir, String classFilePath,
 			String contributionUri, String compsiteUri) {
 		String result = null;
-		result = UpdateContextPayload.OPERATION_TYPE + ":" + operationType + ","
-				+ UpdateContextPayload.COMP_IDENTIFIER + ":" + targetCompIdentifier
-				+ "," + UpdateContextPayload.BASE_DIR + ":" + baseDir + ","
-				+ UpdateContextPayload.CLASS_FILE_PATH + ":" + classFilePath + ","
-				+ UpdateContextPayload.CONTRIBUTION_URI + ":" + contributionUri + ","
-				+ UpdateContextPayload.COMPOSITE_URI + ":" + compsiteUri;
+		result = UpdateContextPayload.OPERATION_TYPE + ":" + operationType
+				+ "," + UpdateContextPayload.COMP_IDENTIFIER + ":"
+				+ targetCompIdentifier + "," + UpdateContextPayload.BASE_DIR
+				+ ":" + baseDir + "," + UpdateContextPayload.CLASS_FILE_PATH
+				+ ":" + classFilePath + ","
+				+ UpdateContextPayload.CONTRIBUTION_URI + ":" + contributionUri
+				+ "," + UpdateContextPayload.COMPOSITE_URI + ":" + compsiteUri;
 		return result;
 	}
 
@@ -50,8 +51,10 @@ public class UpdateContextPayloadCreator {
 		StringBuffer result = new StringBuffer(createPayload(operationType,
 				targetCompIdentifier, baseDir, classFilePath, contributionUri,
 				compsiteUri));
-		result.append(",").append(UpdateContextPayload.SCOPE).append(":").append(scope.toString());
-		
+		if (scope != null)
+			result.append(",").append(UpdateContextPayload.SCOPE).append(":")
+					.append(scope.toString());
+
 		return result.toString();
 	}
 
@@ -65,8 +68,26 @@ public class UpdateContextPayloadCreator {
 	public static String createPayload(UpdateOperationType operationType,
 			String targetCompIdentifier) {
 		String result = null;
-		result = UpdateContextPayload.OPERATION_TYPE + ":" + operationType + ","
-				+ UpdateContextPayload.COMP_IDENTIFIER + ":" + targetCompIdentifier;
+		result = UpdateContextPayload.OPERATION_TYPE + ":" + operationType
+				+ "," + UpdateContextPayload.COMP_IDENTIFIER + ":"
+				+ targetCompIdentifier;
 		return result;
+	}
+	
+	public static String createPayload(UpdateOperationType operationType) {
+		String result = null;
+		result = UpdateContextPayload.OPERATION_TYPE + ":" + operationType;
+		return result;
+	}
+
+	public static String createPayload(UpdateOperationType operationType,
+			String targetCompIdentifier, Scope scope) {
+		StringBuffer result = new StringBuffer(createPayload(operationType,
+				targetCompIdentifier));
+		if (scope != null)
+			result.append(",").append(UpdateContextPayload.SCOPE).append(":")
+					.append(scope.toString());
+
+		return result.toString();
 	}
 }
