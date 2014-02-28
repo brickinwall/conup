ebdfb2b80d4ea6def753719b706c95e1b3eb2b6d
rgc.nju.cs@gmail.com <rgc.nju.cs@gmail.com@3cfdaae8-dac9-9527-6ad1-29de82a2e67e>
2014-02-28 07:05:21 +0000

    ExpSetting changes: add Scope field ExpXMLUtil changes: get scope from ExpSetting.xml

--- trunk/modules/conup-tuscany-extension/src/main/java/cn/edu/nju/moon/conup/ext/utils/experiments/utils/ExpXMLUtil.java
+++ trunk/modules/conup-tuscany-extension/src/main/java/cn/edu/nju/moon/conup/ext/utils/experiments/utils/ExpXMLUtil.java
@@ -2,6 +2,7 @@ package cn.edu.nju.moon.conup.ext.utils.experiments.utils;
 
 import java.io.IOException;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -14,7 +15,7 @@ import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
 
 import cn.edu.nju.moon.conup.ext.utils.experiments.model.ExpSetting;
-import cn.edu.nju.moon.conup.spi.exception.ConupException;
+import cn.edu.nju.moon.conup.spi.datamodel.Scope;
 
 /**
  * @author rgc
@@ -24,14 +25,14 @@ public class ExpXMLUtil {
 	private String tuscanyHome;
 	private String conupXmlPath;
 	private String expXmlPath = null;
-	
+
 	Element conupRoot = null;
 	Document conupDoc = null;
 	Element expRoot = null;
 	Document expDoc = null;
 
 	public ExpXMLUtil() {
-		
+
 		String disPath = getDistributionEnvPath();
 		if (disPath != null) {
 			conupXmlPath = disPath + "/Conup.xml";
@@ -54,7 +55,7 @@ public class ExpXMLUtil {
 			}
 		}
 	}
-	
+
 	public ExpXMLUtil(String xmlPath) {
 		SAXBuilder sb = new SAXBuilder();
 		try {
@@ -73,22 +74,22 @@ public class ExpXMLUtil {
 		return tuscanyHome;
 	}
 
-	public ExpSetting getExpSetting(){
+	public ExpSetting getExpSetting() {
 		ExpSetting expSetting = new ExpSetting();
-//		Element experiment = root.getChild("experiment");
+		// Element experiment = root.getChild("experiment");
 		List settings = expRoot.getChildren("setting");
 		Iterator settingIter = settings.iterator();
 		Element setting = null;
-		while(settingIter.hasNext()){
+		while (settingIter.hasNext()) {
 			Element tmp = (Element) settingIter.next();
-			if(tmp.getAttributeValue("enable").equals("yes")){
+			if (tmp.getAttributeValue("enable").equals("yes")) {
 				setting = tmp;
 				break;
 			}
 		}
-		
+
 		assert setting != null;
-		
+
 		Element indepRun = setting.getChild("indepRun");
 		Element nThreads = setting.getChild("nThreads");
 		Element threadId = setting.getChild("threadId");
@@ -96,79 +97,128 @@ public class ExpXMLUtil {
 		Element ipAddress = setting.getChild("ipAddress");
 		Element rqstInterval = setting.getChild("rqstInterval");
 		Element baseDir = setting.getChild("baseDir");
-		Element type = setting.getChild("type");
-		
+		Element scopeElement = setting.getChild("scope");
+
 		expSetting.setIndepRun(Integer.parseInt(indepRun.getValue().trim()));
 		expSetting.setnThreads(Integer.parseInt(nThreads.getValue().trim()));
 		expSetting.setThreadId(Integer.parseInt(threadId.getValue().trim()));
 		expSetting.setTargetComp(targetComp.getValue().trim());
 		expSetting.setIpAddress(ipAddress.getValue().trim());
-		expSetting.setRqstInterval(Integer.parseInt(rqstInterval.getValue().trim()));
+		expSetting.setRqstInterval(Integer.parseInt(rqstInterval.getValue()
+				.trim()));
 		expSetting.setBaseDir(baseDir.getValue().trim());
-		expSetting.setType(type.getValue().trim());
-		
+
+		if (scopeElement == null) {
+			expSetting.setScope(null);
+		} else {
+			Scope scope = new Scope();
+			List compsInScope = scopeElement.getChildren("component");
+			Iterator compsInScopeIter = compsInScope.iterator();
+			while (compsInScopeIter.hasNext()) {
+				Element comp = (Element) compsInScopeIter.next();
+				String compName = comp.getAttributeValue("name");
+				Set<String> parentComps = new HashSet<String>();
+				Set<String> subComps = new HashSet<String>();
+				
+				List subCompsList = comp.getChildren("child");
+				Iterator iter = subCompsList.iterator();
+				while(iter.hasNext()){
+					Element tmp = (Element)iter.next();
+					subComps.add(tmp.getValue().trim());
+				}
+				
+				List parentCompsList = comp.getChildren("parent");
+				iter = parentCompsList.iterator();
+				while(iter.hasNext()){
+					Element tmp = (Element)iter.next();
+					parentComps.add(tmp.getValue().trim());
+				}
+				scope.addComponent(compName, parentComps, subComps);
+			}
+			Set<String> targetComps = new HashSet<String>();
+			List targetCompsList = scopeElement.getChildren("targetComps");
+			Element targets = null;
+			while(targetCompsList.iterator().hasNext()){
+				targets = (Element)targetCompsList.iterator().next();
+				break;
+			}
+			if(targets != null){
+				List targetsInScope = targets.getChildren("target");
+				Iterator iter = targetsInScope.iterator();
+				while(iter.hasNext()){
+					Element tmp = (Element)iter.next();
+					targetComps.add(tmp.getValue().trim());
+				}
+			}
+			scope.setTarget(targetComps);
+			scope.setSpecifiedScope(true);
+			expSetting.setScope(scope);
+		}
+
 		return expSetting;
 	}
-	
+
 	public String getAlgorithmConf() {
 		Element configuration = conupRoot.getChild("configuration");
 		Element algorithms = configuration.getChild("algorithms");
 		List allALgs = algorithms.getChildren();
 		Iterator algsIter = allALgs.iterator();
 		String algConf = null;
-		while(algsIter.hasNext()){
+		while (algsIter.hasNext()) {
 			Element alg = (Element) algsIter.next();
-			if(alg.getAttributeValue("enable").equals("yes")){
-				algConf =  alg.getValue();
+			if (alg.getAttributeValue("enable").equals("yes")) {
+				algConf = alg.getValue();
 				break;
 			}
 		}
-		
+
 		assert algConf != null;
 		return algConf;
 	}
 
 	public String getFreenessStrategy() {
 		Element configuration = conupRoot.getChild("configuration");
-		Element freenessStrategies = configuration.getChild("freenessStrategies");
+		Element freenessStrategies = configuration
+				.getChild("freenessStrategies");
 		List allStrategies = freenessStrategies.getChildren();
 		Iterator strategiesIter = allStrategies.iterator();
 		String strategyConf = null;
-		while(strategiesIter.hasNext()){
+		while (strategiesIter.hasNext()) {
 			Element strategy = (Element) strategiesIter.next();
-			if(strategy.getAttributeValue("enable").equals("yes")){
-				strategyConf =  strategy.getValue();
+			if (strategy.getAttributeValue("enable").equals("yes")) {
+				strategyConf = strategy.getValue();
 				break;
 			}
 		}
-		
+
 		assert strategyConf != null;
 		return strategyConf;
 	}
 
 	/**
 	 * GET TUSCANY_HOME'S PHYSICAL LOCATION
+	 * 
 	 * @return
 	 */
-	public String getDistributionEnvPath(){
+	public String getDistributionEnvPath() {
 		Properties prpt = System.getProperties();
-		Enumeration<?> enm = prpt.propertyNames(); 
+		Enumeration<?> enm = prpt.propertyNames();
 		String OS = null;
 		while (enm.hasMoreElements()) {
 			String key = (String) enm.nextElement();
-			if(key.equals("os.name")){
+			if (key.equals("os.name")) {
 				OS = System.getProperty(key, "undefined");
 				break;
 			}
 		}
-		
+
 		String disPath = "";
-		Map<String, String> map = System.getenv(); 
-		Set<String> set = map.keySet(); 
+		Map<String, String> map = System.getenv();
+		Set<String> set = map.keySet();
 		Iterator<String> itr = set.iterator();
 		while (itr.hasNext()) {
 			String key = (String) itr.next();
-			if(key.equals("TUSCANY_HOME")){
+			if (key.equals("TUSCANY_HOME")) {
 				String path = map.get(key);
 				tuscanyHome = path;
 				disPath = path + "/bin";
